e9a87b08c0f9b9d44349d8c416a2a92862da9be4
Egor Chuprov <ytdrenk@gmail.com>
2025-11-18 16:48:13 +0300

    test ok

--- 2_selection_sort/selection_sort.go
+++ 2_selection_sort/selection_sort.go
@@ -1,11 +1,20 @@
 package selectionsort
 
 func SelectionSort(arr []int) {
-
+	for i := range arr {
+		smallest := findSmallest(arr, i)
+		arr[i], arr[smallest] = arr[smallest], arr[i]
+	}
 }
 
-func findSmallest(arr []int) int {
-	var index int
-
-	return index
+func findSmallest(arr []int, index int) int {
+	smallest := arr[index]
+	out := index
+	for ; index < len(arr); index++ {
+		if arr[index] < smallest {
+			smallest = arr[index]
+			out = index
+		}
+	}
+	return out
 }
